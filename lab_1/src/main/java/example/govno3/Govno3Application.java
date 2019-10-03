31aa1f31acbb87ca5d7b1b0732ee4a4bf2f46b84
LeraKulichkina <lerok99@mail.ru>
2019-10-03 13:09:30 +0300

    Наработки с лекции

--- lab_1/src/main/java/example/govno3/Govno3Application.java
+++ lab_1/src/main/java/example/govno3/Govno3Application.java
@@ -1,27 +1,36 @@
 package example.govno3;
 
-import org.apache.commons.io.IOUtils;
-import org.json.JSONObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sun.net.www.protocol.http.HttpURLConnection;
-
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.util.
 
 
 @SpringBootApplication
 public class Govno3Application {
 
-	public Govno3Application() throws IOException {
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Govno3Application.class, args);
 	}
-	JSONObject json = new JSONObject(IOUtils.toString(new URL("https://meduza.io/"), Charset.forName("UTF-8")));
-	/*у меня есть объект json, надо его преобразовать в dom */
+
+	public Govno3Application() throws IOException {
+		//convert to the url
+		String meduzaURL = "https://meduza.io/";
+		URL url = new URL (meduzaURL);
+		HttpURLConnection request = (HttpURLConnection) url.openConnection();
+		request.connect();
+		//conver to json obj
+		JsonParser ourparser = new JsonParser();
+		JsonElement root = ourparser.parse(new InputStreamReader((InputStream)request.getContent()));
+		JsonObject rootobj = root.getAsJsonObject();
+	}
 
 }
